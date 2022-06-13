e357930bdd5bd3a17a1e5229b39ae86a09c5ee52
TheDarkParalda <106527424+TheDarkParalda@users.noreply.github.com>
2022-06-14 00:42:46 +0530

    Update slave_bluetooth.ino

--- slave_bluetooth.ino
+++ slave_bluetooth.ino
@@ -1,11 +1,11 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
+#ifdef AVR
 #include <avr/power.h>
 #endif
 
 #define LED_BACK    5
 #define LED_LEFT    4
-#define LED_RIGHT   6
+#define LED_RIGHT   7
 
 #define LED_COUNT 64
 
@@ -23,7 +23,7 @@ int TX_PIN=1;
 
 SoftwareSerial Slave_BTDevice(RX_PIN,TX_PIN);
 
-int flag_turns_slave;//Left=-1.Right=1.Nothing=0
+char flag_turns_slave;//Left=-1.Right=1.Nothing=0
 void setup() 
 {
   strip_back.begin();
@@ -39,54 +39,67 @@ void setup()
   strip_right.setBrightness(150);
 
   Serial.begin(9600);
-  delay(1000);
+ // delay(1000);
   
-  Slave_BTDevice.begin(38400);
-  flag_turns_slave=0;
+  Slave_BTDevice.begin(9600);
+  flag_turns_slave='0';
 }
 void loop() 
 {  
   if(Slave_BTDevice.available()>0)
-  {flag_turns_slave = Slave_BTDevice.read();
-  Serial.write(flag_turns_slave);
-  delay(10);
-  }
-  
-  
-  if(flag_turns_slave==2)
   {
-    stopStop();
-    delay(3000);
-  }
+    flag_turns_slave = Slave_BTDevice.read();
+    //Serial.println("working");
   
-  if(flag_turns_slave==1)
+  //Serial.println(flag_turns_slave);
+  delay(200);
+  }
+  /*if(flag_turns_slave=='0')
+  {noBlink();}*/
+  
+  
+
+ if(flag_turns_slave=='1')
   {
+    //Serial.print('Right');
     rightBlink();
-    delay(3000);
+   // delay(300);
   }
 
-  else if(flag_turns_slave==-1)
+  else if(flag_turns_slave=='3')
   {
-  
+ //Serial.print("Left"); 
   leftBlink();
-  delay(3000);
+  //delay(300);
     
   }
+  else if(flag_turns_slave=='2')
+  {
+    //Serial.print("Stop");
+    stopStop();
+    //noBlink();
+    //delay(300);
+  }
+ /* else if(flag_turns_slave=='0')
+  {delay(1000);
+    //Serial.print("helo");
+  }*/
+  flag_turns_slave='0';
 }
 
   void rightBlink() {
   for (int i = 0; i < 4; i++){
     colorBlinkersRight(strip_right.Color(255,   100,   0), 50);
-    delay(400);
+    delay(120);
     colorBlinkersRight(strip_right.Color(0,   0,   0), 25);
-    delay(300);
+    delay(60);
   }
 }
 void colorBlinkersRight(uint32_t c, int wait){
-  for(int i = 0; i<=15; i++){
+  for(int i = 0; i<=14; i++){
     strip_right.setPixelColor(i, c);
   }
-  for(int i = 0; i<=15; i++){
+  for(int i = 0; i<=14; i++){
     strip_back.setPixelColor(i, c);
   }
   
@@ -103,16 +116,16 @@ void colorBlinkersRight(uint32_t c, int wait){
 void leftBlink() {
   for (int i = 0; i < 4; i++){
     colorBlinkersLeft(strip_left.Color(255,   100,   0), 50);
-    delay(400);
+    delay(120);
     colorBlinkersLeft(strip_left.Color(0,   0,   0), 25);
-    delay(300);
+    delay(60);
   }
 }
 void colorBlinkersLeft(uint32_t c, int wait){
-  for(int i = 0; i<=15; i++){
+  for(int i = 0; i<=14; i++){
     strip_left.setPixelColor(i, c);
   }
-  for(int i = 16; i<=31; i++){
+  for(int i = 15; i<=29; i++){
     strip_back.setPixelColor(i, c);
   }
   
@@ -124,16 +137,22 @@ void colorBlinkersLeft(uint32_t c, int wait){
   delay(wait);
 }
 
-
+int i;
 void stopStop(){
-  colorStop(strip_left.Color(255, 0, 0), 2000);
+  for(i=0;i<1;i++)
+  {
+  colorStop(strip_left.Color(255, 0, 0), 400);}
+  colorStop(strip_left.Color(0, 0, 0), 20);
+  //colorStop(strip_right.Color(255, 0, 0), 2000);
+  
 }
+
 void colorStop(uint32_t c, int wait){
-  for(int i = 0; i<=15; i++){
+  for(int i = 0; i<=14; i++){
     strip_left.setPixelColor(i, c);
     strip_right.setPixelColor(i, c);
   }
-  for(int i = 0; i<=31; i++){
+  for(int i = 0; i<=29; i++){
     strip_back.setPixelColor(i, c);
   }
   
@@ -144,4 +163,32 @@ void colorStop(uint32_t c, int wait){
   strip_left.clear();
   strip_right.clear();
   strip_back.clear();
+  delay(wait);}
+
+
+
+  void noBlink() {
+  for (int i = 0; i < 4; i++){
+    colorBlinkersno(strip_left.Color(0,   0,   0), 50);
+    colorBlinkersno(strip_right.Color(0,   0,   0), 50);
+    delay(400);
+  }
+}
+void colorBlinkersno(uint32_t c, int wait){
+  for(int i = 0; i<=14; i++){
+    strip_left.setPixelColor(i, c);
+    strip_right.setPixelColor(i, c);
+  }
+  for(int i = 0; i<=29; i++){
+    strip_back.setPixelColor(i, c);
+  }
+  
+  strip_left.show();
+  strip_right.show();
+  strip_back.show();
+  delay(wait);
+  strip_left.clear();
+  strip_right.clear();
+  strip_back.clear();
+  delay(wait);
 }
