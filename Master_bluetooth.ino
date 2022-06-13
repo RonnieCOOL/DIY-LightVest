5fbcd07a30e263f3bcd69748ad2ba7de6b773ae0
TheDarkParalda <106527424+TheDarkParalda@users.noreply.github.com>
2022-06-14 00:42:10 +0530

    Update Master_bluetooth.ino

--- Master_bluetooth.ino
+++ Master_bluetooth.ino
@@ -1,15 +1,22 @@
-#define x A0
-#define y A1
-#define z A2
+#include "Wire.h" // This library allows you to communicate with I2C devices.
 
-int Some_specific_Angle_at_which_one_turns=30;
-int x_val;
-int y_val;
-int z_val;
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
-int x_val2;
-int y_val2;
-int z_val2;
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+  }
+
+int MAX_GYROSCOPE_TILT_VALUE_po = 5000; //I'm assuming that both are like positive(right) and negative(left), else we can shift them ig.
+int MAX_GYROSCOPE_TILT_VALUE_ne = -5000;
+int GYROSCOPE_ZERO=0;
+int Some_specific_Angle_at_which_one_turns=4000;
 
 int Gyrovalue;
 int Accvalue;
@@ -21,90 +28,83 @@ int TX_PIN=1;
 
 SoftwareSerial Master_BTDevice(RX_PIN,TX_PIN);
 
-int flag_turns_master;//Left=-1.Right=1.Nothing=0
+int flag_turns_master;//Left=3.Right=1.Nothing=0
 
 void setup() 
 {
-  pinMode(x, INPUT);
-  pinMode(y, INPUT);
-  pinMode(z, INPUT);
-  Serial.begin(9600);
-   
-x_val2 = analogRead(x);
-  y_val2 = analogRead(y);
-  z_val2 = analogRead(z);
   
-  Master_BTDevice.begin(38400);
+  
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Wire.begin();
+  Serial.begin(9600);
+  Master_BTDevice.begin(9600);
 }
 
 void loop() {
 
-
-  x_val = analogRead(x);
-
-int Gyrovalue;
-
-int Accvalue;
-
-
-
-int RX_PIN=0;
-
-int TX_PIN=1;
-
-
-
-#include<SoftwareSerial.h>
-
-
-
-SoftwareSerial Master_BTDevice(RX_PIN,TX_PIN);
-  y_val = analogRead(y);
-  z_val = analogRead(z);
-
-  int x_adc_value = x_val;
-  int y_adc_value = y_val;
-  int z_adc_value = z_val;
-
-  x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in x-direction in g units */ 
-  y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in y-direction in g units */ 
-  z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); /* Acceleration in z-direction in g units */ 
-
-  int yaw =  ( ( (atan2(x_g_value,y_g_value) * 180) / 3.14 ) + 180 );
+Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) 
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
   
-  Serial.print("| aX = "); Serial.print(x_adc_value);
-  Serial.print(" | aY = "); Serial.print((y_adc_value));
-  Serial.print(" | aZ = "); Serial.print((z_adc_value));
-  Serial.print(" yaw = "); Serial.print((yaw));
-  Serial.println();
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+ // Serial.print("aX = "); Serial.print((accelerometer_x));
+  //Serial.print(" | aY = "); Serial.print((accelerometer_y));
+ // Serial.print(" | aZ = "); Serial.print((accelerometer_z));
+ // Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  //Serial.print(" | gX = "); Serial.print((gyro_x));
+  //Serial.print(" | gY = "); Serial.print((gyro_y));
+  //Serial.print(" | gZ = "); Serial.print((gyro_z));
+  //Serial.println();
   
   // delay
-  delay(1000);
-  
-  Gyrovalue=yaw;
-  Accvalue=x_g_value;
-  
-if(Accvalue<0)
-{
-  flag_turns_master=2;
-  Master_BTDevice.print(2);
-}
+  delay(100);
+  Gyrovalue=gyro_z;
+  Accvalue=accelerometer_x;
+  //Serial.print(Accvalue);
 
+//Gyrovalue=(Gyrovalue-GYROSCOPE_ZERO)*180/(MAX_GYROSCOPE_TILT_VALUE_po-MAX_GYROSCOPE_TILT_VALUE_ne);
 if(Gyrovalue>Some_specific_Angle_at_which_one_turns)
 {
   flag_turns_master=1;    
-  Master_BTDevice.print(1);
+  Serial.print('1');
+  delay(2000);
+  //Serial.print("Right");
 }
 else if(Gyrovalue<-Some_specific_Angle_at_which_one_turns)
 {
-  flag_turns_master=-1;    
-  Master_BTDevice.print(-1);
+  flag_turns_master=3;    
+  Serial.print('3');
+  delay(2000);
+  //Serial.print("Left");
 }
+if(Accvalue<-2700)
+{
+  flag_turns_master=2;
+  Serial.print('2');
+  delay(2000);
+  //Serial.print("Stop");
+}
+
 else
 {
   flag_turns_master=0;    
-  Master_BTDevice.print(0);
+  Serial.print('0');
+  delay(2000);
 }
 
-
+flag_turns_master=0;
 }
