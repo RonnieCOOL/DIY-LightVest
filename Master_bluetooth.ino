8ac7455608211bb007e9a8b1da4551baf0a70fc0
TheDarkParalda <106527424+TheDarkParalda@users.noreply.github.com>
2022-06-14 15:33:50 +0530

    Update Master_bluetooth.ino

--- Master_bluetooth.ino
+++ Master_bluetooth.ino
@@ -44,14 +44,14 @@ Wire.beginTransmission(MPU_ADDR);
   Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
   Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
   
-  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  accelerometer_x = Wire.read()<<8 | Wire.read(); 
+  accelerometer_y = Wire.read()<<8 | Wire.read();
+  accelerometer_z = Wire.read()<<8 | Wire.read(); 
+  temperature = Wire.read()<<8 | Wire.read(); 
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read(); 
+  gyro_z = Wire.read()<<8 | Wire.read(); 
   
   // print out data
  // Serial.print("aX = "); Serial.print((accelerometer_x));
